7e62c0c9272711d827b815dda8aa130c4e3f5227
d0nguk <40707277+d0nguk@users.noreply.github.com>
2021-09-15 17:05:40 +0900

    Q4
    Q4

---
+++ Nexon_CodeTest/Q4/Q4.cpp
@@ -0,0 +1,19 @@
+#include "Q4.h"
+
+void Solution_Q4()
+{
+	vector<string> map;
+	int map_len;
+
+	cin >> map_len;
+	string str;
+	for (int i = 0; i < map_len; ++i)
+	{
+		cin >> str;
+		map.push_back(str);
+	}
+
+#pragma region CODE
+	// 여기에 코드를 입력하세요
+#pragma endregion
+}
---
+++ Nexon_CodeTest/Q4/Q4.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "stdafx.h"
+
+void Solution_Q4();
